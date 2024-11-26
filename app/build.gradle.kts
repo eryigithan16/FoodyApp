bf0eaccce9cdf7295bd5d7225fb808a64c6e5fa8
eryigithan16 <ygthnerygt1615@outlook.com>
2024-11-26 18:12:22 +0300

    splash screen created

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
 }
 
 dependencies {
-
+    //noinspection UseTomlInstead
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -53,4 +53,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
