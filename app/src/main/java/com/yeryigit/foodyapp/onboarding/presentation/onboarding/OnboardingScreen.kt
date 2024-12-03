d37484ce70b6ff1bd98f25bd486b813937b98fdd
eryigithan16 <ygthnerygt1615@outlook.com>
2024-12-03 17:25:59 +0300

    onboarding screen started

---
+++ app/src/main/java/com/yeryigit/foodyapp/onboarding/presentation/onboarding/OnboardingScreen.kt
@@ -0,0 +1,69 @@
+package com.yeryigit.foodyapp.onboarding.presentation.onboarding
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.yeryigit.foodyapp.R
+import com.yeryigit.foodyapp.ui.theme.FoodyAppTheme
+
+@Composable
+fun OnboardingScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                imageVector = ImageVector.vectorResource(R.drawable.ic_foody),
+                contentDescription = "Icon 1",
+                modifier = Modifier.size(30.dp)
+            )
+            Box(
+                modifier = Modifier
+                    .align(Alignment.CenterEnd)
+            ) {
+                Text(
+                    text = "Skip",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun OnBoardingScreenPreview() {
+    FoodyAppTheme {
+        OnboardingScreen()
+    }
+}
