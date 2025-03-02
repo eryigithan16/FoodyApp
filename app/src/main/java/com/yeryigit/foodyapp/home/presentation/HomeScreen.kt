3236bbe5fbb997c2707cd79784b523395ad63e64
eryigithan16 <ygthnerygt1615@outlook.com>
2025-03-02 23:54:39 +0300

    home screen and header view created

---
+++ app/src/main/java/com/yeryigit/foodyapp/home/presentation/HomeScreen.kt
@@ -0,0 +1,10 @@
+package com.yeryigit.foodyapp.home.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.yeryigit.foodyapp.ui.components.HeaderView
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    HeaderView()
+}
---
+++ app/src/main/java/com/yeryigit/foodyapp/ui/components/HeaderView.kt
@@ -0,0 +1,9 @@
+package com.yeryigit.foodyapp.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HeaderView(modifier: Modifier = Modifier) {
+
+}
